d03c2b2dd6e3661baec13161a690317fef430662
admin2001-coder <dywong@global.tencent.com>
2026-03-26 14:28:53 -0700

    added a new function

---
+++ functions/csig_example/lib.rs
@@ -0,0 +1,10 @@
+extern crate taubyte_sdk;
+use taubyte_sdk::event::Event;
+
+#[no_mangle]
+pub fn tencent(event: Event) -> u32 {
+    let http = event.http().unwrap();
+    _ = http.write("hello tencent".as_bytes()).unwrap();
+
+    0
+}
